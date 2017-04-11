45aab6de8da8acb04af15e6c0732dd0817c7cbad
ashja1 <ashja@live.co.uk>
2017-04-11 11:53:55 +0100

    Adding Build Folder

---
+++ build/src/CMakeFiles/mst.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mst.dir/mstsolver.o"
+  "CMakeFiles/mst.dir/krushkal.o"
+  "CMakeFiles/mst.dir/prim.o"
+  "mst.pdb"
+  "mst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/testmst.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testmst.dir/test_mstsolvers.o"
+  "CMakeFiles/testmst.dir/krushkal.o"
+  "CMakeFiles/testmst.dir/prim.o"
+  "CMakeFiles/testmst.dir/filehandling.o"
+  "testmst.pdb"
+  "testmst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testmst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
